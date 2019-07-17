134212ede88416f2476756cf045d0b4b66ce0b45
Yang Liu <liuyang12@focusmedia.cn>
2019-07-17 18:38:42 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# EAST
+# EAST: An Efficient and Accurate Scene Text Detector
+
+This is a PyTorch re-implementation of EAST: An Efficient and Accurate Scene Text Detector.
 
 ## Dependency
 
 - PyTorch 1.1.0
+
+## Usage
+### Data Pre-processing
+Extract training images:
+```bash
+$ python extract.py
+$ python pre_process.py
+```
+
+### Train
+```bash
+$ python train.py
+```
+
+If you want to visualize during training, run in your terminal:
+```bash
+$ tensorboard --logdir runs
+```
--- data_gen.py
+++ data_gen.py
@@ -6,6 +6,7 @@ import cv2 as cv
 import numpy as np
 from torch.utils.data import Dataset
 from torchvision import transforms
+
 from config import im_size
 
 # Data augmentation and normalization for training
@@ -25,17 +26,15 @@ data_transforms = {
 
 def load_annoataion(p):
     text_polys = []
-    text_tags = []
-    if not os.path.exists(p):
-        return np.array(text_polys, dtype=np.float32)
+
     with open(p, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         for line in reader:
             line = [i.strip('\ufeff') for i in line]
-            x1, y1, x2, y2, x3, y3, x4, y4 = list(map(int, line[:8]))
+            x1, y1, x2, y2, x3, y3, x4, y4 = list(line[:8])
             text_polys.append([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
 
-    return np.array(text_polys, dtype=np.float32), np.array(text_tags, dtype=np.bool)
+    return np.array(text_polys, dtype=np.float32)
 
 
 class EastDataset(Dataset):
@@ -56,18 +55,13 @@ class EastDataset(Dataset):
         h, w = img.shape[:2]
         ratio_x = float(im_size) / w
         ratio_y = float(im_size) / h
+        img = cv.resize(img, dsize=(im_size, im_size))
         img = img[..., ::-1]  # RGB
         img = transforms.ToPILImage()(img)
         img = self.transformer(img)
         text_polys = load_annoataion(txt_fn)
-        img = cv.resize(img, dsize=(im_size, im_size))
-
-        # print(text_polys.type.name)
-
-        for i in range(len(text_polys)):
-            for j in range(4):
-                text_polys[i][j][0] *= ratio_x
-                text_polys[i][j][1] *= ratio_y
+        text_polys[:, :, 0] *= ratio_x
+        text_polys[:, :, 1] *= ratio_y
 
         return img, text_polys
 
