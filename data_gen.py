5628059b3118102dadc5427dc41991e2a6a456ab
Yang Liu <liuyang12@focusmedia.cn>
2019-07-18 17:44:06 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -112,6 +112,58 @@ class EastDataset(Dataset):
 
 
 if __name__ == "__main__":
+    import random
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as Patches
+
     dataset = EastDataset('test')
-    score_map = dataset[0][1]
+    length = len(dataset)
+    index = random.randint(0, length - 1)
+
+    im = dataset[index][0]
+    score_map = dataset[index][1]
+    geo_map = dataset[index][2]
+    training_mask = dataset[index][3]
     print(score_map.shape)
+
+    fig, axs = plt.subplots(3, 2, figsize=(20, 30))
+    # axs[0].imshow(im[:, :, ::-1])
+    # axs[0].set_xticks([])
+    # axs[0].set_yticks([])
+    # for poly in text_polys:
+    #     poly_h = min(abs(poly[3, 1] - poly[0, 1]), abs(poly[2, 1] - poly[1, 1]))
+    #     poly_w = min(abs(poly[1, 0] - poly[0, 0]), abs(poly[2, 0] - poly[3, 0]))
+    #     axs[0].add_artist(Patches.Polygon(
+    #         poly * 4, facecolor='none', edgecolor='green', linewidth=2, linestyle='-', fill=True))
+    #     axs[0].text(poly[0, 0] * 4, poly[0, 1] * 4, '{:.0f}-{:.0f}'.format(poly_h * 4, poly_w * 4),
+    #                    color='purple')
+    # axs[1].imshow(score_map)
+    # axs[1].set_xticks([])
+    # axs[1].set_yticks([])
+    axs[0, 0].imshow(im[:, :, ::-1])
+    axs[0, 0].set_xticks([])
+    axs[0, 0].set_yticks([])
+    for poly in text_polys:
+        poly_h = min(abs(poly[3, 1] - poly[0, 1]), abs(poly[2, 1] - poly[1, 1]))
+        poly_w = min(abs(poly[1, 0] - poly[0, 0]), abs(poly[2, 0] - poly[3, 0]))
+        axs[0, 0].add_artist(Patches.Polygon(
+            poly, facecolor='none', edgecolor='green', linewidth=2, linestyle='-', fill=True))
+        axs[0, 0].text(poly[0, 0], poly[0, 1], '{:.0f}-{:.0f}'.format(poly_h, poly_w), color='purple')
+    axs[0, 1].imshow(score_map[::, ::])
+    axs[0, 1].set_xticks([])
+    axs[0, 1].set_yticks([])
+    axs[1, 0].imshow(geo_map[::, ::, 0])
+    axs[1, 0].set_xticks([])
+    axs[1, 0].set_yticks([])
+    axs[1, 1].imshow(geo_map[::, ::, 1])
+    axs[1, 1].set_xticks([])
+    axs[1, 1].set_yticks([])
+    axs[2, 0].imshow(geo_map[::, ::, 2])
+    axs[2, 0].set_xticks([])
+    axs[2, 0].set_yticks([])
+    axs[2, 1].imshow(training_mask[::, ::])
+    axs[2, 1].set_xticks([])
+    axs[2, 1].set_yticks([])
+    plt.tight_layout()
+    plt.show()
+    plt.close()
