2560c1fe07981342eba582c9f0d7ba4c7c853b3a
Yang Liu <liuyang12@focusmedia.cn>
2019-07-19 05:28:54 +0800

    update

--- utils.py
+++ utils.py
@@ -86,7 +86,7 @@ def parse_args():
     parser.add_argument('--input-size', type=int, default=1000, help='image input size.')
     parser.add_argument('--batch-size-per-gpu', type=int, default=14, help='batch size in each context')
     parser.add_argument('--num-gpus', type=int, default=2, help='number of gpus')
-    parser.add_argument('--lr', type=float, default=0.0001, help='start learning rate')
+    parser.add_argument('--lr', type=float, default=0.001, help='start learning rate')
     parser.add_argument('--lr-step', type=int, default=10, help='period of learning rate decay')
     parser.add_argument('--optimizer', default='sgd', help='optimizer')
     parser.add_argument('--weight-decay', type=float, default=0.0, help='weight decay')
