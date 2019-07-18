69837d034c75f9708564026c5c72a7e8bd7ad3f7
Yang Liu <liuyang12@focusmedia.cn>
2019-07-18 14:19:01 +0800

    update

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
+import numpy as np
 import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # sets device for model and PyTorch tensors
 
-im_size = 512
+input_size = 512
 training_data_path = 'data'  # training dataset to use
 max_image_large_side = 1280  # max image size of training
 max_text_size = 800  # if the text in the input image is bigger than this, then we resize the image according to this
 min_text_size = 10  # if the text size is smaller than this, we ignore it during training
 min_crop_side_ratio = 0.1  # when doing random crop from input image, the min length of min(H, W)
 geometry = 'RBOX'  # which geometry to generate, RBOX or QUAD
+background_ratio = 3. / 8
+random_scale = np.array([0.5, 1, 2.0, 3.0])
 
 train_ratio = 0.9
 num_samples = 1000
--- data_gen.py
+++ data_gen.py
@@ -5,8 +5,8 @@ import numpy as np
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-from config import im_size, training_data_path
-from icdar import load_annoataion, get_images
+from config import input_size, training_data_path, background_ratio, random_scale, geometry
+from icdar import load_annoataion, get_images, check_and_validate_polys, crop_area, generate_rbox
 
 # Data augmentation and normalization for training
 # Just normalization for validation
@@ -34,25 +34,63 @@ class EastDataset(Dataset):
         self.transformer = data_transforms[split]
 
     def __getitem__(self, i):
-        sample = self.samples[i]
-        im_fn = os.path.join('data', sample['img'])
-        txt_fn = os.path.join('data', sample['gt'])
-        img = cv.imread(im_fn)
-        h, w = img.shape[:2]
-        ratio_x = float(im_size) / w
-        ratio_y = float(im_size) / h
-        img = cv.resize(img, dsize=(im_size, im_size))
-        img = img[..., ::-1]  # RGB
-        img = transforms.ToPILImage()(img)
-        img = self.transformer(img)
-        text_polys = load_annoataion(txt_fn)
-        text_polys[:, :, 0] *= ratio_x
-        text_polys[:, :, 1] *= ratio_y
+        im_fn = self.image_list[i]
+        im = cv.imread(im_fn)
+        # print im_fn
+        h, w, _ = im.shape
+        txt_fn = im_fn.replace(os.path.basename(im_fn).split('.')[1], 'txt')
+        assert (os.path.exists(txt_fn))
 
-        print(img.size())
-        print(text_polys.shape)
+        text_polys, text_tags = load_annoataion(txt_fn)
 
-        return img, text_polys
+        text_polys, text_tags = check_and_validate_polys(text_polys, text_tags, (h, w))
+        # if text_polys.shape[0] == 0:
+        #     continue
+        # random scale this image
+        rd_scale = np.random.choice(random_scale)
+        im = cv.resize(im, dsize=None, fx=rd_scale, fy=rd_scale)
+        text_polys *= rd_scale
+        # print rd_scale
+        # random crop a area from image
+        if np.random.rand() < background_ratio:
+            # crop background
+            im, text_polys, text_tags = crop_area(im, text_polys, text_tags, crop_background=True)
+            assert (text_polys.shape[0] > 0)
+            # pad and resize image
+            new_h, new_w, _ = im.shape
+            max_h_w_i = np.max([new_h, new_w, input_size])
+            im_padded = np.zeros((max_h_w_i, max_h_w_i, 3), dtype=np.uint8)
+            im_padded[:new_h, :new_w, :] = im.copy()
+            im = cv.resize(im_padded, dsize=(input_size, input_size))
+            score_map = np.zeros((input_size, input_size), dtype=np.uint8)
+            geo_map_channels = 5 if geometry == 'RBOX' else 8
+            geo_map = np.zeros((input_size, input_size, geo_map_channels), dtype=np.float32)
+            training_mask = np.ones((input_size, input_size), dtype=np.uint8)
+        else:
+            im, text_polys, text_tags = crop_area(im, text_polys, text_tags, crop_background=False)
+            assert (text_polys.shape[0] == 0)
+
+            h, w, _ = im.shape
+
+            # pad the image to the training input size or the longer side of image
+            new_h, new_w, _ = im.shape
+            max_h_w_i = np.max([new_h, new_w, input_size])
+            im_padded = np.zeros((max_h_w_i, max_h_w_i, 3), dtype=np.uint8)
+            im_padded[:new_h, :new_w, :] = im.copy()
+            im = im_padded
+            # resize the image to input size
+            new_h, new_w, _ = im.shape
+            resize_h = input_size
+            resize_w = input_size
+            im = cv.resize(im, dsize=(resize_w, resize_h))
+            resize_ratio_3_x = resize_w / float(new_w)
+            resize_ratio_3_y = resize_h / float(new_h)
+            text_polys[:, :, 0] *= resize_ratio_3_x
+            text_polys[:, :, 1] *= resize_ratio_3_y
+            new_h, new_w, _ = im.shape
+            score_map, geo_map, training_mask = generate_rbox((new_h, new_w), text_polys, text_tags)
+
+        return im, score_map, geo_map, training_mask
 
     def __len__(self):
         return len(self.image_list)
