292678066e9c05107b842e00567ca1fdae4a3ce3
Yang Liu <liuyang12@focusmedia.cn>
2019-07-19 09:59:16 +0800

    update

--- train.py
+++ train.py
@@ -58,9 +58,9 @@ def train_net(args):
     # Epochs
     for epoch in range(start_epoch, args.end_epoch):
         # Decay learning rate if there is no improvement for 2 consecutive epochs, and terminate training after 10
-        if epochs_since_improvement == 10:
+        if epochs_since_improvement == 20:
             break
-        if epochs_since_improvement > 0 and epochs_since_improvement % 2 == 0:
+        if epochs_since_improvement > 0 and epochs_since_improvement % 8 == 0:
             checkpoint = 'BEST_checkpoint.tar'
             checkpoint = torch.load(checkpoint)
             model = checkpoint['model']
