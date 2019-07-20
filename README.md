1908ed537e4321d746b15a3418acb74d91689a29
unknown <foamliu@yeah.net>
2019-07-20 12:38:43 +0800

    update

--- README.md
+++ README.md
@@ -34,6 +34,8 @@ cd ../../..
 mv data/result/epoch_0_gt/submit.zip ./script_test_ch4
 cd script_test_ch4
 python3 script.py –g=gt.zip –s=submit.zip
+cd ..
+
 </pre>
 
 ## Dependency
--- script_test_ch4/rrc_evaluation_funcs.py
+++ script_test_ch4/rrc_evaluation_funcs.py
@@ -303,7 +303,7 @@ def main_evaluation(p,default_evaluation_params_fn,validate_data_fn,evaluate_met
         evalData = evaluate_method_fn(p['g'], p['s'], evalParams)
         resDict.update(evalData)
         
-    except Exception, e:
+    except Exception as e:
         resDict['Message']= str(e)
         resDict['calculated']=False
 
