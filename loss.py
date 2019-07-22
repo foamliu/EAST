883d960f5da8f715a353860b782b83a275c98892
Yang Liu <liuyang12@focusmedia.cn>
2019-07-22 10:14:03 +0800

    update

--- loss.py
+++ loss.py
@@ -2,46 +2,41 @@ import torch
 import torch.nn as nn
 
 
-def dice_coefficient(y_true_cls, y_pred_cls,
-                     training_mask):
-    '''
-    dice loss
-    :param y_true_cls:
-    :param y_pred_cls:
-    :param training_mask:
-    :return:
-    '''
-    eps = 1e-5
-    intersection = torch.sum(y_true_cls * y_pred_cls * training_mask)
-    union = torch.sum(y_true_cls * training_mask) + torch.sum(y_pred_cls * training_mask) + eps
-    loss = 1. - (2 * intersection / union)
-
-    return loss
+def get_dice_loss(gt_score, pred_score):
+    inter = torch.sum(gt_score * pred_score)
+    union = torch.sum(gt_score) + torch.sum(pred_score) + 1e-5
+    return 1. - (2 * inter / union)
 
 
-class LossFunc(nn.Module):
-    def __init__(self):
-        super(LossFunc, self).__init__()
-        return
+def get_geo_loss(gt_geo, pred_geo):
+    d1_gt, d2_gt, d3_gt, d4_gt, angle_gt = torch.split(gt_geo, 1, 1)
+    d1_pred, d2_pred, d3_pred, d4_pred, angle_pred = torch.split(pred_geo, 1, 1)
+    area_gt = (d1_gt + d2_gt) * (d3_gt + d4_gt)
+    area_pred = (d1_pred + d2_pred) * (d3_pred + d4_pred)
+    w_union = torch.min(d3_gt, d3_pred) + torch.min(d4_gt, d4_pred)
+    h_union = torch.min(d1_gt, d1_pred) + torch.min(d2_gt, d2_pred)
+    area_intersect = w_union * h_union
+    area_union = area_gt + area_pred - area_intersect
+    iou_loss_map = -torch.log((area_intersect + 1.0) / (area_union + 1.0))
+    angle_loss_map = 1 - torch.cos(angle_pred - angle_gt)
+    return iou_loss_map, angle_loss_map
 
-    def forward(self, y_true_cls, y_pred_cls, y_true_geo, y_pred_geo, training_mask):
-        classification_loss = dice_coefficient(y_true_cls, y_pred_cls, training_mask)
-        # scale classification loss to match the iou loss part
-        classification_loss *= 0.01
 
-        # d1 -> top, d2->right, d3->bottom, d4->left
-        #     d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
-        d1_gt, d2_gt, d3_gt, d4_gt, theta_gt = torch.split(y_true_geo, 1, 1)
-        #     d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
-        d1_pred, d2_pred, d3_pred, d4_pred, theta_pred = torch.split(y_pred_geo, 1, 1)
-        area_gt = (d1_gt + d3_gt) * (d2_gt + d4_gt)
-        area_pred = (d1_pred + d3_pred) * (d2_pred + d4_pred)
-        w_union = torch.min(d2_gt, d2_pred) + torch.min(d4_gt, d4_pred)
-        h_union = torch.min(d1_gt, d1_pred) + torch.min(d3_gt, d3_pred)
-        area_intersect = w_union * h_union
-        area_union = area_gt + area_pred - area_intersect
-        L_AABB = -torch.log((area_intersect + 1.0) / (area_union + 1.0))
-        L_theta = 1 - torch.cos(theta_pred - theta_gt)
-        L_g = L_AABB + 20 * L_theta
+class Loss(nn.Module):
+    def __init__(self, weight_angle=10):
+        super(Loss, self).__init__()
+        self.weight_angle = weight_angle
 
-        return torch.mean(L_g * y_true_cls * training_mask) + classification_loss
+    def forward(self, gt_score, pred_score, gt_geo, pred_geo, ignored_map):
+        if torch.sum(gt_score) < 1:
+            return torch.sum(pred_score + pred_geo) * 0
+
+        classify_loss = get_dice_loss(gt_score, pred_score * (1 - ignored_map))
+        iou_loss_map, angle_loss_map = get_geo_loss(gt_geo, pred_geo)
+
+        angle_loss = torch.sum(angle_loss_map * gt_score) / torch.sum(gt_score)
+        iou_loss = torch.sum(iou_loss_map * gt_score) / torch.sum(gt_score)
+        geo_loss = self.weight_angle * angle_loss + iou_loss
+        print('classify loss is {:.8f}, angle loss is {:.8f}, iou loss is {:.8f}'.format(classify_loss, angle_loss,
+                                                                                         iou_loss))
+        return geo_loss + classify_loss
