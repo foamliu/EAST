117640cc9b3d05fce2f11ccf4f1299aa2ca05103
Yang Liu <liuyang12@focusmedia.cn>
2019-07-24 15:36:49 +0800

    udpate

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ def train_net(args):
 
     # Custom dataloaders
     train_dataset = EastDataset('train')
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size_per_gpu * args.num_gpus,
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size,
                                                shuffle=True, num_workers=num_workers, collate_fn=collate_fn)
     # test_dataset = EastDataset('test')
     # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size_per_gpu * args.num_gpus,
