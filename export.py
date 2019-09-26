07ccb066c4d6ca9d841641031f947892568f433a
Yang Liu <liuyang12@focusmedia.cn>
2019-09-26 16:41:38 +0800

    update

---
+++ export.py
@@ -0,0 +1,39 @@
+import time
+
+import torch
+
+from models import EastModel
+
+if __name__ == '__main__':
+    checkpoint = 'BEST_checkpoint.tar'
+    print('loading {}...'.format(checkpoint))
+    start = time.time()
+    checkpoint = torch.load(checkpoint)
+    print('elapsed {} sec'.format(time.time() - start))
+    model = checkpoint['model'].module
+    print(type(model))
+    print('use_se: ' + str(model.use_se))
+    print('fc: ' + str(model.fc))
+
+    # model.eval()
+    filename = 'insight-face-v3.pt'
+    print('saving {}...'.format(filename))
+    start = time.time()
+    torch.save(model.state_dict(), filename)
+    print('elapsed {} sec'.format(time.time() - start))
+
+
+    class HParams:
+        def __init__(self):
+            self.pretrained = True
+            self.network = 'r50'
+
+
+    config = HParams()
+
+    print('loading {}...'.format(filename))
+    start = time.time()
+
+    model = EastModel(config)
+    model.load_state_dict(torch.load(filename))
+    print('elapsed {} sec'.format(time.time() - start))
